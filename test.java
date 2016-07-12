81ec08b2eed2bfb7771f4b6f2d0e115a8fb70033
corrado cardone <corrado.cardone@gmail.com>
2016-07-12 08:35:53 +0200

    commit unziped files

---
+++ test.java
@@ -0,0 +1,12 @@
+folder container for backed file
+test send this file to remote repository
+test change this file commmit and push (1)
+***test change this file commmit and push (2)
+***test change this file commmit and push (3)
+***test change this file commmit and push (4)
+***test change this file commmit and push (5)
+***test change this file commmit and push (6)
+***test change this file commmit and push (7)
+***test unziped file commmit and push
+***test change this file commmit and push (8)
+***test change clone command from git Camel component
